1661c0cc9141de06ce5f0276f7c86629c02be2bf
krllkrnv <krllkrnv@yandex.ru>
2023-11-02 21:14:07 +0300

    Added header and footer, testing table
    Added header and footer, testing table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <v-app>
+      <header-component><main-page/></header-component>
+      <footer-component/>
+    </v-app>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+import mainPage from "./pages/mainPage.vue";
+import FooterComponent from "./components/footerComponent.vue";
+import headerComponent from "./components/headerComponent.vue";
+export default {
+  name: 'App',
+  components: {headerComponent, FooterComponent, mainPage},
+
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ frontend/src/components/headerComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-card variant="text">
+      <v-layout>
+        <v-app-bar elevation="0"
+                   color="#FA5FA2"
+                   prominent
+        >          <v-spacer></v-spacer>
+
+          <v-toolbar-title>Сводная таблицы анализа данных</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+
+        </v-app-bar>
+
+
+
+        <v-main>
+          <v-app><slot></slot></v-app>
+        </v-main>
+      </v-layout>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
