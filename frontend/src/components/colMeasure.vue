f2a55bf7ce47dbcdbacd563dab26f8cf5ffd28cc
krllkrnv <krllkrnv@yandex.ru>
2023-11-05 04:03:48 +0300

    Add and delete logic, minor styling, table draw

--- frontend/src/components/colMeasure.vue
+++ frontend/src/components/colMeasure.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <v-card>Измерения по столбцам</v-card>
-  </div>
+  <v-card variant="outlined">Измерения по столбцам
+    <v-card v-for="elem in colData"
+            class="v-card-element ma-5" width="100%" variant="tonal"
+            max-width="500"
+    >{{elem}}
+      <v-card-title>{{metaData.find(item => item.id === elem.fieldId).name}}</v-card-title>
+      <v-card-actions>
+        <v-btn @click="this.$store.commit('DELETE_COL_MEASURE', elem.fieldId)" color="error" variant="tonal">Удалить</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-card>
 </template>
 
 <script>
 import {mapGetters} from "vuex";
 
 export default {
-
+computed: {
+  ...mapGetters(['COl_MEASURE', 'META_DATA']),
+  colData(){
+    return this.COl_MEASURE
+  },
+  metaData(){
+    return this.META_DATA
+  }
+},
 }
 </script>
 
